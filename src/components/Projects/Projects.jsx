b96a7fe0e49b3fba5c855607b9e2b82f2c168e18
tweppy <tweppy@gmail.com>
2023-10-31 17:13:19 +0100

    update projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,15 +8,25 @@ const Projects = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {projects.map((project, id) => {
           return (
-            <div className="bg-gray-800 p-4 rounded-lg" key={id}>
-              <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
-              <p className="mb-6">{project.description}</p>
-              <a className="border-violet-500 border-2 rounded-3xl font-semibold px-4 py-2 hover:bg-violet-500" href={project.source} target="_blank" rel="noreferrer">Code</a>
-              <ul className="flex flex-row gap-2 pt-4 flex-wrap">
-                {project.skills.map((skill, id) => {
-                  return <li className="bg-violet-500 rounded-3xl font-semibold px-4 py-2" key={id}>{skill}</li>;
-                })}
-              </ul>
+            <div className="bg-gray-800 p-4 rounded-lg flex flex-col justify-between" key={id}>
+              <div>
+                <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
+                <p className="mb-6">{project.description}</p>
+              </div>
+              <div>
+                <a className="border-violet-500 border-2 rounded-3xl font-semibold px-4 py-2 hover:bg-violet-500" href={project.source} target="_blank" rel="noreferrer">
+                  Code
+                </a>
+                <ul className="flex flex-row gap-2 pt-4 flex-wrap">
+                  {project.skills.map((skill, id) => {
+                    return (
+                      <li className="bg-violet-500 rounded-3xl font-semibold px-4 py-2" key={id}>
+                        {skill}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
           );
         })}
@@ -26,19 +36,3 @@ const Projects = () => {
 };
 
 export default Projects;
-
-/*
-- Todo react
-- e-wallet
-
-- this portfolio website
-
-- latest aws
-- swing notes
-- another backend thing ?
-
-- redo shopping react style and products and add that? but I can't asnewer questions about it...
-- add onto movie-search...? check chatgpt
-
-
-*/
